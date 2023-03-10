687849406c017d5af9f42eb347dc9d376008a0b3
Olga Mikhailova <foxx_fire@mail.ru>
2023-03-10 22:37:19 +0300

    Initial Commit

---
+++ iOS8 - HW10 - Mikhailova Olga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS8 - HW10 - Mikhailova Olga
+//
+//  Created by mbpro2.0/16/512 on 10.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
